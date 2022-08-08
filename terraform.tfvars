b614f0782a8795b3437a6354b96c96d3d99559fd
kodjo <kaziamble2001@gmail.com>
2022-08-08 17:17:26 -0500

    terraform code

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+region        = "us-east-1"
+ami           = "ami-090fa75af13c156b4"
+keypair       = "awskeypair"
+instance-type = "t2.nano"
---
+++ variables.tf
@@ -0,0 +1,18 @@
+
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "ami" {
+    type = string
+    default = "ami-090fa75af13c156b4"
+}
+variable "instance-type" {
+    type = string
+    default ="t2.micro"
+}
+variable "keypair" {
+    type = string
+    default ="keypair"
+}
